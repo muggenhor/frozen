16977113195d67bd94c882d210a77e67ccb63661
Giel van Schijndel <giel@mortis.eu>
2022-08-01 20:10:21 +0200

    feat: add empty(), front(), back() to frozen::string matching std::string_view

--- include/frozen/string.h
+++ include/frozen/string.h
@@ -23,6 +23,7 @@
 #ifndef FROZEN_LETITGO_STRING_H
 #define FROZEN_LETITGO_STRING_H
 
+#include "frozen/bits/constexpr_assert.h"
 #include "frozen/bits/elsa.h"
 #include "frozen/bits/hash_string.h"
 #include "frozen/bits/version.h"
@@ -60,6 +61,7 @@ public:
   constexpr basic_string(const basic_string &) noexcept = default;
   constexpr basic_string &operator=(const basic_string &) noexcept = default;
 
+  constexpr bool empty() const { return !size_; }
   constexpr std::size_t size() const { return size_; }
 
   constexpr value_type operator[](std::size_t i) const { return data_[i]; }
@@ -90,6 +92,16 @@ public:
   constexpr const value_type *data() const { return data_; }
   constexpr const value_type *begin() const { return data(); }
   constexpr const value_type *end() const { return data() + size(); }
+
+  constexpr const value_type &front() const noexcept {
+    constexpr_assert(!empty(), "empty string");
+    return data_[0];
+  }
+
+  constexpr const value_type &back() const noexcept {
+    constexpr_assert(!empty(), "empty string");
+    return data_[size() - 1];
+  }
 };
 
 template <typename _CharT> struct elsa<basic_string<_CharT>> {
